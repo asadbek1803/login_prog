4c75b8a0113ef00f0534ca9690eb15eb6be714a1
Asadbek <97333773+asadbek1803@users.noreply.github.com>
2022-01-15 09:56:03 +0500

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 12:16:02 2022
+
+@author: User 4
+"""
+
+# loginlar = ["oyatillo", "salohiddin" , "asadbek", "abdulvoris", "ruxshona"]
+# login = input("Yangi login tanlang\n>>>>: ")
+# if login not in loginlar:
+#     print("Xush kelibsiz")
+# else:
+#     print("Login band, yangin login tanlang!")
+    
